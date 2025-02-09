aae2c9ad7d2705891aa4ed5c1caa7d5db92ec065
Mo Zhu <79839921+zzmmzzm@users.noreply.github.com>
2025-02-09 23:43:41 +0800

    Update filter_papers.py

--- filter_papers.py
+++ filter_papers.py
@@ -5,7 +5,10 @@ import re
 from typing import List
 
 import retry
+import openai
+print(openai.__version__)
 from openai import OpenAI
+import openai
 from tqdm import tqdm
 
 from arxiv_scraper import Paper
@@ -63,7 +66,7 @@ def calc_price(model, usage):
 
 
 @retry.retry(tries=3, delay=2)
-def call_deepseek_api(full_prompt, openai_client, model):
+def call_chatgpt(full_prompt, openai_client, model):
     return openai_client.chat.completions.create(
         model=model,
         messages=[{"role": "user", "content": full_prompt}],
@@ -72,29 +75,34 @@ def call_deepseek_api(full_prompt, openai_client, model):
     )
 
 
-def run_and_parse_deepseek_api(full_prompt, openai_client, config):
-    # just runs the deepseek prompt, tries to parse the resulting JSON
-    completion = call_deepseek_api(full_prompt, openai_client, config["SELECTION"]["model"])
+
+def run_and_parse_chatgpt(full_prompt, openai_client, config):
+    # just runs the chatgpt prompt, tries to parse the resulting JSON
+    completion = call_chatgpt(full_prompt, openai_client, config["SELECTION"]["model"])
     out_text = completion.choices[0].message.content
     out_text = re.sub("```jsonl\n", "", out_text)
     out_text = re.sub("```", "", out_text)
     out_text = re.sub(r"\n+", "\n", out_text)
     out_text = re.sub("},", "}", out_text).strip()
-
+    # split out_text line by line and parse each as a json.
     json_dicts = []
     for line in out_text.split("\n"):
+        # try catch block to attempt to parse json
         try:
             json_dicts.append(json.loads(line))
         except Exception as ex:
             if config["OUTPUT"].getboolean("debug_messages"):
-                print(f"Exception: {ex}")
-                print("Failed to parse output as JSON")
+                print("Exception happened " + str(ex))
+                print("Failed to parse LM output as json")
                 print(out_text)
+                print("RAW output")
+                print(completion.choices[0].message.content)
             continue
     return json_dicts, calc_price(config["SELECTION"]["model"], completion.usage)
 
 
 def paper_to_string(paper_entry: Paper) -> str:
+    # renders each paper into a string to be processed by GPT
     new_str = (
         "ArXiv ID: "
         + paper_entry.arxiv_id
@@ -112,6 +120,7 @@ def paper_to_string(paper_entry: Paper) -> str:
 
 
 def batched(items, batch_size):
+    # takes a list and returns a list of list with batch_size
     return [items[i : i + batch_size] for i in range(0, len(items), batch_size)]
 
 
@@ -128,7 +137,7 @@ def filter_papers_by_title(
             base_prompt + "\n " + criterion + "\n" + papers_string + filter_postfix
         )
         model = config["SELECTION"]["model"]
-        completion = call_deepseek_api(full_prompt, openai_client, model)
+        completion = call_chatgpt(full_prompt, openai_client, model)
         cost += calc_price(model, completion.usage)
         out_text = completion.choices[0].message.content
         try:
@@ -139,8 +148,8 @@ def filter_papers_by_title(
                 else:
                     print("Filtered out paper " + paper.arxiv_id)
         except Exception as ex:
-            print(f"Exception: {ex}")
-            print("Failed to parse output as list " + out_text)
+            print("Exception happened " + str(ex))
+            print("Failed to parse LM output as list " + out_text)
             print(completion)
             continue
     return final_list, cost
@@ -162,33 +171,39 @@ def run_on_batch(
             postfix_prompt,
         ]
     )
-    json_dicts, cost = run_and_parse_deepseek_api(full_prompt, openai_client, config)
+    json_dicts, cost = run_and_parse_chatgpt(full_prompt, openai_client, config)
     return json_dicts, cost
 
 
 def filter_by_gpt(
     all_authors, papers, config, openai_client, all_papers, selected_papers, sort_dict
 ):
+    # deal with config parsing
     with open("configs/base_prompt.txt", "r") as f:
         base_prompt = f.read()
     with open("configs/paper_topics.txt", "r") as f:
         criterion = f.read()
     with open("configs/postfix_prompt.txt", "r") as f:
         postfix_prompt = f.read()
-    
     all_cost = 0
     if config["SELECTION"].getboolean("run_openai"):
+        # filter first by hindex of authors to reduce costs.
         paper_list = filter_papers_by_hindex(all_authors, papers, config)
         if config["OUTPUT"].getboolean("debug_messages"):
-            print(f"{len(paper_list)} papers after hindex filtering")
+            print(str(len(paper_list)) + " papers after hindex filtering")
         cost = 0
         paper_list, cost = filter_papers_by_title(
             paper_list, config, openai_client, base_prompt, criterion
         )
         if config["OUTPUT"].getboolean("debug_messages"):
-            print(f"{len(paper_list)} papers after title filtering with cost of ${cost}")
+            print(
+                str(len(paper_list))
+                + " papers after title filtering with cost of $"
+                + str(cost)
+            )
         all_cost += cost
 
+        # batch the remaining papers and invoke GPT
         batch_of_papers = batched(paper_list, int(config["SELECTION"]["batch_size"]))
         scored_batches = []
         for batch in tqdm(batch_of_papers):
@@ -199,7 +214,8 @@ def filter_by_gpt(
             all_cost += cost
             for jdict in json_dicts:
                 if (
-                    int(jdict["RELEVANCE"]) >= int(config["FILTERING"]["relevance_cutoff"])
+                    int(jdict["RELEVANCE"])
+                    >= int(config["FILTERING"]["relevance_cutoff"])
                     and jdict["NOVELTY"] >= int(config["FILTERING"]["novelty_cutoff"])
                     and jdict["ARXIVID"] in all_papers
                 ):
@@ -220,7 +236,92 @@ def filter_by_gpt(
                 config["OUTPUT"]["output_path"] + "gpt_paper_batches.debug.json", "w"
             ) as outfile:
                 json.dump(scored_batches, outfile, cls=EnhancedJSONEncoder, indent=4)
-        
         if config["OUTPUT"].getboolean("debug_messages"):
-            print(f"Total cost: ${all_cost}")
+            print("Total cost: $" + str(all_cost))
 
+
+if __name__ == "__main__":
+    OAI_KEY = os.environ.get("OAI_KEY2")
+    if OAI_KEY is None:
+        raise ValueError(
+            "OpenAI key is not set - please set OAI_KEY to your OpenAI key"
+        )
+        
+    client = OpenAI(api_key=OAI_KEY, base_url="https://api.deepseek.com")
+    
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "user", "content": "Hello"},
+        ],
+        stream=False,
+        temperature=0,
+        seed=0
+    )
+    
+    print(response.choices[0].message.content)
+    config = configparser.ConfigParser()
+    config.read("configs/config.ini")
+    # now load the api keys
+    keyconfig = configparser.ConfigParser()
+    keyconfig.read("configs/keys.ini")
+    #S2_API_KEY = keyconfig["KEYS"]["semanticscholar"]
+    openai_client = OpenAI(api_key=OAI_KEY, base_url="https://api.deepseek.com")
+    #openai_client = openai
+    # deal with config parsing
+    with open("configs/base_prompt.txt", "r") as f:
+        base_prompt = f.read()
+    with open("configs/paper_topics.txt", "r") as f:
+        criterion = f.read()
+    with open("configs/postfix_prompt.txt", "r") as f:
+        postfix_prompt = f.read()
+    # loads papers from 'in/debug_papers.json' and filters them
+    with open("in/debug_papers.json", "r") as f:
+        # with open("in/gpt_paper_batches.debug-11-10.json", "r") as f:
+        paper_list_in_dict = json.load(f)
+    papers = [
+        [
+            Paper(
+                arxiv_id=paper["arxiv_id"],
+                authors=paper["authors"],
+                title=paper["title"],
+                abstract=paper["abstract"],
+            )
+            for paper in batch
+        ]
+        for batch in paper_list_in_dict
+    ]
+    all_papers = {}
+    paper_outputs = {}
+    sort_dict = {}
+    total_cost = 0
+    for batch in tqdm(papers):
+        json_dicts, cost = run_on_batch(
+            batch, base_prompt, criterion, postfix_prompt, openai_client, config
+        )
+        total_cost += cost
+        for paper in batch:
+            all_papers[paper.arxiv_id] = paper
+        for jdict in json_dicts:
+            paper_outputs[jdict["ARXIVID"]] = {
+                **dataclasses.asdict(all_papers[jdict["ARXIVID"]]),
+                **jdict,
+            }
+            sort_dict[jdict["ARXIVID"]] = jdict["RELEVANCE"] + jdict["NOVELTY"]
+
+        # sort the papers by relevance and novelty
+    print("total cost:" + str(total_cost))
+    keys = list(sort_dict.keys())
+    values = list(sort_dict.values())
+
+    def argsort(seq):
+        return sorted(range(len(seq)), key=seq.__getitem__)
+
+    sorted_keys = [keys[idx] for idx in argsort(values)[::-1]]
+    selected_papers = {key: paper_outputs[key] for key in sorted_keys}
+
+    with open(
+        config["OUTPUT"]["output_path"] + "filter_paper_test.debug.json", "w"
+    ) as outfile:
+        json.dump(selected_papers, outfile, cls=EnhancedJSONEncoder, indent=4)
